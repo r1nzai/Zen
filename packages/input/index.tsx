7530c917a1a6423c2bfe15ca66dce5075b206826
r1nzai <60702701+r1nzai@users.noreply.github.com>
2024-01-13 03:32:44 +0530

    readonly mode for input

--- packages/input/index.tsx
+++ packages/input/index.tsx
@@ -9,6 +9,7 @@ export default function Input(props: InputProps) {
             className={cx(
                 'h-10 w-full rounded border-2 border-input bg-background px-3 py-2 text-sm text-foreground transition',
                 'focus-visible:border-primary focus-visible:shadow-sm focus-visible:shadow-ring focus-visible:outline-none',
+                'read-only:cursor-pointer read-only:!border-none read-only:bg-transparent read-only:text-foreground read-only:!shadow-none read-only:!outline-none',
                 'disabled:cursor-not-allowed disabled:bg-muted disabled:text-muted',
                 'file:border-0 file:bg-transparent file:text-sm file:font-medium',
                 className,
