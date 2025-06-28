bc579c3058e6148d0465933ee98d9cd5e1a528cb
r1nzai <parameswarkishan99@gmail.com>
2025-06-28 21:37:55 +0530

    toggle buttton alignment fix?

--- packages/toggle/index.tsx
+++ packages/toggle/index.tsx
@@ -12,7 +12,7 @@ export default function Toggle(props: ToggleProps) {
             className={cx(
                 'rounded-full',
                 'h-5 w-10',
-                'border border-muted bg-secondary',
+                'border-muted bg-secondary border',
                 'flex items-center',
                 className,
             )}
@@ -21,8 +21,8 @@ export default function Toggle(props: ToggleProps) {
             <input type="hidden" readOnly {...rest} {...{ defaultChecked, checked }} />
             <div
                 className={cx(
-                    '[animation-timing-function:cubic-bezier(1, 0, 0, 1)] duration-250 h-4 w-4 rounded-full bg-primary transition-all',
-                    checked ? 'translate-x-5' : 'translate-x-1',
+                    '[animation-timing-function:cubic-bezier(1, 0, 0, 1)] bg-primary size-4 rounded-full transition-all duration-250',
+                    checked ? 'translate-x-5' : 'translate-x-0',
                 )}
             />
         </div>
